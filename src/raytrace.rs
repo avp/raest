6dde1f8706b959a3b635e9b414278a0a95cb8c17
Aakash Patel <aakpat6@gmail.com>
2020-05-25 21:32:39 -0700

    Iterative UDPT.

--- src/raytrace.rs
+++ src/raytrace.rs
@@ -1,3 +1,4 @@
+#[allow(unused_imports)]
 use crate::bdpt::BDPT;
 use crate::camera::Camera;
 use crate::color::Color;
@@ -27,7 +28,7 @@ fn raytrace_rows(
     // Buffer the rows before writing them to `buf` to avoid too much contention
     // on the lock.
     let mut row_backlog: Vec<(usize, Vec<u32>)> = vec![];
-    let mut tracer = BDPT::new(scene);
+    let mut tracer = UDPT::new(scene);
     for r in rows {
         let mut row: Vec<u32> = vec![0; config.width];
         for (c, result) in row.iter_mut().enumerate() {
--- src/udpt.rs
+++ src/udpt.rs
@@ -17,62 +17,82 @@ impl<'scene> UDPT<'scene> {
     }
 
     pub fn ray_color(&self, ray: Ray, depth: u32) -> Color {
-        if depth > MAX_DEPTH {
+        if depth == 0 {
             return Color::zeros();
         }
-        match self.scene.hit(ray, 0.0001..f64::INFINITY) {
-            Some(hit) => {
-                let emit = hit.material.emitted(&hit);
-                match hit.material.scatter(&ray, &hit) {
-                    None => emit,
-                    Some(scatter) => {
-                        if let Some(specular) = scatter.specular {
-                            return scatter.attenuation.component_mul(
-                                &self.ray_color(specular, depth + 1),
-                            );
+        let mut ray = ray;
+        let mut result = Color::new(1.0, 1.0, 1.0);
+        for _ in 0..depth {
+            match self.scene.hit(ray, 0.0001..f64::INFINITY) {
+                Some(hit) => {
+                    let emit = hit.material.emitted(&hit);
+                    match hit.material.scatter(&ray, &hit) {
+                        None => {
+                            result = result.component_mul(&emit);
+                            break;
                         }
-                        if let Some(scatter_pdf) = scatter.pdf {
-                            let light_pdf =
-                                PDF::hittable(hit.point, &self.scene.lights);
-                            let final_pdf = if self.scene.lights.is_empty() {
-                                scatter_pdf
-                            } else {
-                                PDF::mix(0.75, &scatter_pdf, &light_pdf)
-                            };
-                            let scatter_ray = Ray {
-                                origin: hit.point,
-                                dir: final_pdf.gen(),
-                            };
-                            let color = self.ray_color(scatter_ray, depth + 1);
-                            // The final value is the emission plus the MC
-                            // estimate: attenuation
-                            // * color(dir) * (s(dir) / p(dir))
-                            // where `s` is the scattering PDF and `p` is the
-                            // PDF we used to generate the direction.
-                            // In Lambertian surfaces without MIS, for example,
-                            // s(dir) and p(dir) are both cos(theta) where theta
-                            // is the angle between the normal and dir, so those
-                            // ordinarily cancel.
-                            // Adding MIS mixes the final_pdf which we used to
-                            // generate the scatter_ray, which throws off the
-                            // calculation so we actually do need to do the
-                            // division now.
-                            return emit
-                                + scatter.attenuation.component_mul(&color)
+                        Some(scatter) => {
+                            if let Some(specular) = scatter.specular {
+                                result =
+                                    result.component_mul(&scatter.attenuation);
+                                ray = specular;
+                            }
+                            if let Some(scatter_pdf) = scatter.pdf {
+                                let light_pdf = PDF::hittable(
+                                    hit.point,
+                                    &self.scene.lights,
+                                );
+                                let final_pdf = if self.scene.lights.is_empty()
+                                {
+                                    scatter_pdf
+                                } else {
+                                    PDF::mix(0.75, &scatter_pdf, &light_pdf)
+                                };
+                                let scatter_ray = Ray {
+                                    origin: hit.point,
+                                    dir: final_pdf.gen(),
+                                };
+                                // The final value is the emission plus the MC
+                                // estimate: attenuation
+                                // * color(dir) * (s(dir) / p(dir))
+                                // where `s` is the scattering PDF and `p` is
+                                // the
+                                // PDF we used to generate the direction.
+                                // In Lambertian surfaces without MIS, for
+                                // example,
+                                // s(dir) and p(dir) are both cos(theta) where
+                                // theta
+                                // is the angle between the normal and dir, so
+                                // those
+                                // ordinarily cancel.
+                                // Adding MIS mixes the final_pdf which we used
+                                // to
+                                // generate the scatter_ray, which throws off
+                                // the
+                                // calculation so we actually do need to do the
+                                // division now.
+                                result = result
+                                    .component_mul(&scatter.attenuation)
                                     * scatter_pdf.value(scatter_ray.dir)
                                     * final_pdf.value(scatter_ray.dir).recip();
+                                ray = scatter_ray;
+                            }
+                            Color::zeros()
                         }
-                        Color::zeros()
                     }
                 }
-            }
-            None => self.scene.background,
+                None => {
+                    result = result.component_mul(&self.scene.background);
+                    break;
+                }
+            };
         }
+        result
     }
 }
 
 impl<'scene> Tracer for UDPT<'scene> {
     fn sample(&mut self, ray: Ray, _debug: bool) -> Color {
-        self.ray_color(ray, 0)
+        self.ray_color(ray, MAX_DEPTH)
     }
 }
